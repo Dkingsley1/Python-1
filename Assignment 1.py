408531a8fa34ba7d08b66165be7d2b131f54166b
Dkingsley1 <dan_kingsley@aol.com>
2025-11-30 14:35:00 -0500

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,9 @@
+principle = float(input("enter principle amount:"))
+
+rate = float(input("enter interest rate (in percentage):"))
+
+time = float(input("enter time period (in years):"))
+
+simple_interest = (principle * rate * time) / 100
+
+print("The caluclated simple interest is: ", simple_interest)
